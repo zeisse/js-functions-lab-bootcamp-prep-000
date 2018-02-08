9e873db77e5cd47013c86277e3dbc7d6a8f5a598
Brook Zeisse <b@zeisse.com>
2018-02-08 23:02:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(days, holiday) {
-  return `It's ${days} until ${holiday}!`
+  return `It's ${days} days until ${holiday}!`
 }
